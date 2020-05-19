ec256d4e4cc63c31c2723429977a3e9a44b6002c
ecmnet <ecm@gmx.de>
2020-05-19 06:28:39 +0200

    SpeedExpo added

--- mavcontrol/src/main/java/com/comino/mavcontrol/autopilot/AutoPilotBase.java
+++ mavcontrol/src/main/java/com/comino/mavcontrol/autopilot/AutoPilotBase.java
@@ -478,8 +478,8 @@ public abstract class AutoPilotBase implements Runnable, ITargetListener {
 		if(enable) {
 			model.sys.setStatus(Status.MSP_JOY_ATTACHED,true);
 			body_speed.set(
-					p == 0 && r == 0 ? Float.NaN : p * 1.5f,
-					p == 0 && r == 0 ? Float.NaN : r * 1.5f,
+					p == 0 && r == 0 ? Float.NaN : MSPMathUtils.expo(p,0.3f) * 2f,
+					p == 0 && r == 0 ? Float.NaN : MSPMathUtils.expo(r,0.3f) * 2f,
 					h == 0 ? Float.NaN : h,
 					y == 0 ? Float.NaN : y);
 
