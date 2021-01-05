a46ea1bb9884a0bfb4a8bebe3bc38b3a0714e282
ecmnet <ecm@gmx.de>
2021-01-05 08:06:22 +0100

    Copyright adjusted

--- mavcontrol/src/main/java/com/comino/mavcontrol/autopilot/CollisonPreventionPilot.java
+++ mavcontrol/src/main/java/com/comino/mavcontrol/autopilot/CollisonPreventionPilot.java
@@ -2,7 +2,7 @@ package com.comino.mavcontrol.autopilot;
 
 /****************************************************************************
 *
-*   Copyright (c) 2017,2020 Eike Mansfeld ecm@gmx.de. All rights reserved.
+*   Copyright (c) 2017,2021 Eike Mansfeld ecm@gmx.de. All rights reserved.
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions
