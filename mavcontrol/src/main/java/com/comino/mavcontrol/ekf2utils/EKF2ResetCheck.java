d66321714d9c14d0823211b523235e48537d9e03
ecmnet <ecm@gmx.de>
2022-08-12 15:36:43 +0200

    Calculate offset based on odometry message

--- mavcontrol/src/main/java/com/comino/mavcontrol/ekf2utils/EKF2ResetCheck.java
+++ mavcontrol/src/main/java/com/comino/mavcontrol/ekf2utils/EKF2ResetCheck.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.mavlink.messages.MAV_SEVERITY;
+import org.mavlink.messages.lquac.msg_local_position_ned;
 import org.mavlink.messages.lquac.msg_msp_ekf2_reset;
 import org.mavlink.messages.lquac.msg_odometry;
 
@@ -20,13 +21,13 @@ public class EKF2ResetCheck implements IMAVLinkListener {
 	private final List<Runnable> listener = new ArrayList<Runnable>();
 	private final IMAVController control;
 	private final DataModel model;
-	
+
 	private float local_pos_x = 0;
 	private float local_pos_y = 0;
 	private float local_pos_z = 0;
-	
+
 	private int counter = 0;
-	
+
 	private final msg_msp_ekf2_reset reset_msg = new msg_msp_ekf2_reset(2,1);
 
 	public EKF2ResetCheck(IMAVController control) {
@@ -39,35 +40,36 @@ public class EKF2ResetCheck implements IMAVLinkListener {
 	public void addListener(Runnable r) {
 		listener.add(r);
 	}
-	
+
 	public void reset() {
-		
+
 		this.counter = 0;
-		
+
 		model.state.l_rx = 0;
 		model.state.l_ry = 0;
 		model.state.l_rz = 0;
-		
+
 		local_pos_x = 0;
 		local_pos_y = 0;
 		local_pos_z = 0;
-		
-	}
 
+	}
+	
 	@Override
 	public void received(Object o) {
 		msg_odometry odom = (msg_odometry) o;
+
 		if(reset_counter_old != odom.reset_counter) {
 			reset_counter_old = odom.reset_counter;	
-			
+
 			// calculate offset between previous lpos and new one
-			model.state.l_rx = model.state.l_x - local_pos_x;
-			model.state.l_ry = model.state.l_y - local_pos_y;
-			model.state.l_rz = model.state.l_z - local_pos_z;
-			
+			model.state.l_rx += (odom.x - local_pos_x);
+			model.state.l_ry += (odom.y - local_pos_y);
+			model.state.l_rz += (odom.z - local_pos_z);
+
 			// Run listener
 			listener.forEach((r) -> r.run());
-			
+
 			// Send MSP message to GC
 			reset_msg.offset_x = model.state.l_rx;
 			reset_msg.offset_y = model.state.l_ry;
@@ -75,14 +77,16 @@ public class EKF2ResetCheck implements IMAVLinkListener {
 			reset_msg.counter  = ++counter;
 			reset_msg.tms = DataModel.getSynchronizedPX4Time_us();
 			control.sendMAVLinkMessage(reset_msg);
-			
+
 			control.writeLogMessage(new LogMessage("[msp] EKF2 reset detected.", MAV_SEVERITY.MAV_SEVERITY_DEBUG)); 
-			
+
 		} else {
 			
-			local_pos_x = model.state.l_x;
-			local_pos_y = model.state.l_y;
-			local_pos_z = model.state.l_z;
+			local_pos_x = odom.x;
+			local_pos_y = odom.y;
+			local_pos_z = odom.z;
+			
 		}
 	}	
+
 }
