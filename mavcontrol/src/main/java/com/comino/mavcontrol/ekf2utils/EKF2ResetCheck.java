c2f157c2b1cb182cafe20ac24d57dd5ea71957db
ecmnet <ecm@gmx.de>
2022-08-12 09:30:50 +0200

    Calculate reset offset and store it in model

--- mavcontrol/src/main/java/com/comino/mavcontrol/ekf2utils/EKF2ResetCheck.java
+++ mavcontrol/src/main/java/com/comino/mavcontrol/ekf2utils/EKF2ResetCheck.java
@@ -8,18 +8,27 @@ import org.mavlink.messages.lquac.msg_odometry;
 
 import com.comino.mavcom.control.IMAVController;
 import com.comino.mavcom.mavlink.IMAVLinkListener;
+import com.comino.mavcom.model.DataModel;
 import com.comino.mavcom.model.segment.LogMessage;
+
+import georegression.struct.point.Vector3D_F64;
 
 public class EKF2ResetCheck implements IMAVLinkListener {
 
 	private int   reset_counter_old = 0;
 	private final List<Runnable> listener = new ArrayList<Runnable>();
 	private final IMAVController control;
+	private final DataModel model;
+	
+	private float local_pos_x = 0;
+	private float local_pos_y = 0;
+	private float local_pos_z = 0;
 
 	public EKF2ResetCheck(IMAVController control) {
 		this.control = control;
+		this.model = control.getCurrentModel();
 		control.addMAVLinkListener(msg_odometry.class, this);
-		System.out.println("Reset counter check initilized..");
+		System.out.println("EKF2 reset counter check initialized..");
 	}
 
 	public void addListener(Runnable r) {
@@ -31,8 +40,21 @@ public class EKF2ResetCheck implements IMAVLinkListener {
 		msg_odometry odom = (msg_odometry) o;
 		if(reset_counter_old != odom.reset_counter) {
 			reset_counter_old = odom.reset_counter;	
+			
+			// calculate offset between previous lpos and new one
+			model.state.l_rx = model.state.l_x - local_pos_x;
+			model.state.l_ry = model.state.l_y - local_pos_y;
+			model.state.l_rz = model.state.l_z - local_pos_z;
+			
+			// Run listener
 			listener.forEach((r) -> r.run());
+			
 			control.writeLogMessage(new LogMessage("[msp] EKF2 reset detected.", MAV_SEVERITY.MAV_SEVERITY_DEBUG)); 
-		}	
+		} else {
+			
+			local_pos_x = model.state.l_x;
+			local_pos_y = model.state.l_y;
+			local_pos_z = model.state.l_z;
+		}
 	}	
 }
